b6ce703a423b69028003684b5be55300a111e1ad
Chris Ashman <chrisashman96@gmail.com>
2020-02-04 23:55:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,3 +21,19 @@ class Song
   end
   
   def self.artists
+    @@artists.uniq 
+  end
+  
+  def self.artist_count
+    artist_count = {}
+    @@artist.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else 
+        artist_count [artist] = 1 
+      end
+    end
+    artist_count
+  end
+  
+  def self.genre_co
